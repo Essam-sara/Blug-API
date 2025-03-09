f7bf3fd18186d6f0ba357a61ceeb3e77d1886e94
Essam-sara <saraessam2580@gmail.com>
2025-03-09 16:40:17 +0200

    Fix: Added user signup, login, and authorization routes

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/users");
+const auth = require("../middlewares/auth");  // استيراد الميدلوير auth
 
-// GET /users -> get all users
-router.get("/", usersController.getUsers);
-// POST /users -> create a new user
-router.post("/", usersController.createUser);
+
+// POST /users/signup -> make a new user
+// POST /users/login -> user login 
+router.post("/signup", usersController.signup);
+router.post("/login", usersController.login);
+
+
+const restrictTo = require("../middlewares/restrictTo");
+
+//only amin can gain the list of users 
+router.get("/", auth, restrictTo("admin"), usersController.getUsers);
+
+
 // GET /users/:id -> get a user by id
 router.get("/:id", usersController.getUserById);
 // PUT /users/:id -> update a user by id
